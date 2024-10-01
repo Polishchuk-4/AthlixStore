6b6a57a1743df224395ee477551d11af1055e680
Bohdan Taran <bohdantaran.work@gmail.com>
2024-10-01 14:14:16 +0200

    ui: add input component

---
+++ src/components/Input/Input.tsx
@@ -0,0 +1,35 @@
+import { InputHTMLAttributes, ReactNode, useMemo } from 'react';
+
+import cn from 'classnames';
+import styles from './Input.module.scss';
+
+export interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
+  placeholder?: string;
+  className?: string;
+  inputSize?: 'large' | 'medium' | 'small';
+  variant?: 'default' | 'success' | 'error' | 'warning';
+  icon?: ReactNode;
+}
+
+const Input = ({
+  placeholder,
+  inputSize = 'medium',
+  variant = 'default',
+  icon,
+  className,
+  ...restProps
+}: InputProps) => {
+  const classNames = useMemo(
+    () => cn(styles.input, styles[inputSize], styles[variant], className),
+    [className, inputSize, variant],
+  );
+
+  return (
+    <div className={styles.wrapper}>
+      <span className={styles.icon}>{icon}</span>
+      <input className={classNames} type="text" placeholder={placeholder} {...restProps} />
+    </div>
+  );
+};
+
+export default Input;
--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,9 +1,12 @@
+import Icon from '../../components/Icon/Icon';
+import Input from '../../components/Input/Input';
 import style from './HomePage.module.scss';
 
 export default function HomePage() {
   return (
     <main className={style.main}>
       <p>HomePage</p>
+      <Input placeholder="Password" icon={<Icon icon="eye-on" />} />
     </main>
   );
 }
