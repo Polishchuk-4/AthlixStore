93abef201d712c1c3b19b760ade889dc9ec5817a
Bohdan Taran <bohdantaran.work@gmail.com>
2024-10-02 14:58:54 +0200

    ui: add logic with password input type

--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -1,7 +1,8 @@
-import { InputHTMLAttributes, ReactNode, useMemo } from 'react';
+import { InputHTMLAttributes, ReactNode, useMemo, useState } from 'react';
 
 import cn from 'classnames';
 import styles from './Input.module.scss';
+import Icon from '../Icon/Icon';
 
 export interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
   placeholder?: string;
@@ -9,6 +10,7 @@ export interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
   inputSize?: 'large' | 'medium' | 'small';
   variant?: 'default' | 'success' | 'error' | 'warning';
   icon?: ReactNode;
+  passwordInput?: boolean;
 }
 
 const Input = ({
@@ -18,19 +20,39 @@ const Input = ({
   icon,
   disabled,
   className,
+  passwordInput = false,
   ...restProps
 }: InputProps) => {
+  const [isPasswordVisible, setIsPasswordVisible] = useState(false);
+
   const classNames = useMemo(
     () => cn(styles.input, styles[inputSize], styles[variant], className),
     [className, inputSize, variant],
   );
 
+  const handleTogglePasswordVisibility = () => {
+    setIsPasswordVisible((prev) => !prev);
+  };
+
+  const inputType = useMemo(() => {
+    if (passwordInput) {
+      return isPasswordVisible ? 'text' : 'password';
+    }
+    return restProps.type || 'text';
+  }, [isPasswordVisible, restProps.type, passwordInput]);
+
   return (
     <div className={styles.wrapper}>
-      <span className={styles.icon}>{icon}</span>
+      {passwordInput ? (
+        <span className={styles.passwordIcon} onClick={handleTogglePasswordVisibility}>
+          {isPasswordVisible ? <Icon icon="eye-on" /> : <Icon icon="eye-off" />}
+        </span>
+      ) : (
+        <span className={styles.icon}>{icon}</span>
+      )}
       <input
         className={classNames}
-        type="text"
+        type={inputType}
         disabled={disabled}
         placeholder={placeholder}
         {...restProps}
