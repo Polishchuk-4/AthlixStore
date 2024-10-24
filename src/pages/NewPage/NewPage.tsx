f36f6ff885a5c156fff700d61c1fc23e8938fc76
Denys Polishchuk <polischyk0000@gmail.com>
2024-10-25 00:51:59 +0300

    remove file

--- src/pages/NewPage/NewPage.tsx
+++ src/pages/NewPage/NewPage.tsx
@@ -1,12 +1,9 @@
-import PreviewSection from '../../components/pages/NewPage/PreviewSection/PreviewSection.tsx';
-
 import style from './NewPage.module.scss';
 
 export default function NewPage() {
   return (
     <main className={style.main}>
       <p>HomePage</p>
-      <PreviewSection />
     </main>
   );
 }
