779251504e27461c0645a8b2fa1371d501c754eb
Denys Polishchuk <polischyk0000@gmail.com>
2024-10-29 15:01:38 +0200

    add log for values

--- src/components/common/MailingForm/MailingForm.tsx
+++ src/components/common/MailingForm/MailingForm.tsx
@@ -18,6 +18,7 @@ export default function MailingForm() {
   const emailFieldId = useId();
 
   const handleSubmit = (values: FormValues, action: FormikHelpers<FormValues>) => {
+    console.log(values);
     action.resetForm();
   };
 
